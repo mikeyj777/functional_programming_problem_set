f006e00e7ea26010fac28548de536dc5416234e1
Michael James <mikeyj777@gmail.com>
2025-02-02 17:57:03 -0500

    getting boiler plate files in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Functional from './components/Functional';
 import './App.css';
 import './styles/global.css';
 
@@ -10,6 +11,7 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/func" element={<Functional />} />
         </Routes>
       </div>
     </Router>
